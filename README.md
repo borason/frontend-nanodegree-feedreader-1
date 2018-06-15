e54bd4edf5ea5f594f365eb79c8e065f9ac30123
Bora Son <btson310@gmail.com>
2018-06-15 11:28:57 -0600

    fix - expands on README

--- README.md
+++ README.md
@@ -16,15 +16,19 @@ For more information about the Udacity Front End Nano Degree, please visit the [
 ## Instructions
 
 - #### Install
-  ###### Bash
+    Navigate to the project repository and dowload the zip file or using the terminal enter the following command:
 
-    ```https://github.com/borason/frontend-nanodegree-feedreader-1.git```
+    ```git clone https://github.com/borason/frontend-nanodegree-feedreader-1.git```
 
-    Navigate to the project directory.
+    Navigate to the project directory and double-click the index.html file or using the terminal enter the following command:
 
     ```open ./index.html```
+
+    Scroll to the bottom of the page to see the results of the tests.
 
 ## Documentation
 
 This project uses the Jasmine Testing Framework.
 For more information refer to the [Jasmine Github v2.1 Documentation](https://jasmine.github.io/2.1/introduction).
+
+This project is used to help the student formulate Jasmine tests on a working feed reader website provided by Udacity. The goal is to create seven total tests that are independent of each other to ensure the website is working properly. The project also provides a SpecRunner HTML file to allow for live browser-based testing results.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,4 +1,4 @@
-
+'use strict';
 $(function() {
 // Test suite for RSS feeds
     describe('RSS Feeds', function() {
@@ -9,89 +9,64 @@ $(function() {
         });
         // Second test loops through all items in the array and checks to see if their is a url defined and contains content
         it('feed urls are defined', function () {
-            for(let i = 0; i < allFeeds.length; i++) {
-              expect(allFeeds[i].url).toBeDefined();
-              expect(allFeeds[i].url.length).not.toBe(0);
-            }
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeTruthy();
+            });
         });
         // Third test loops through the array and checks that there is a feed name and that it has content
         it('feed names are defined', function () {
-            for(let i = 0; i < allFeeds.length; i++) {
-              expect(allFeeds[i].name).toBeDefined();
-              expect(allFeeds[i].name.length).not.toBe(0);
-            }
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeTruthy();
+            });
         });
     });
 // Test suite for the menu
     describe('The Menu', function() {
-        // Variable that selects the hamburger icon
-        const hamburger = document.querySelector('.icon-list');
         // Test that checks to see the menu is hidden by default by making sure the body element has the 'menu-hidden' class
         it('hidden by default', function() {
-            expect(document.body.classList.contains('menu-hidden')).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
         // Test to see that the menu appears and disappears when clicked
         it('toggles menu when clicked', function() {
-        /* simulateClick function was obtained from
-        https://gomakethings.com/how-to-simulate-a-click-event-with-javascript/
-        which is licenced under MIT license https://gomakethings.com/mit */
-           let simulateClick = function (elem) {
-                // Create our event (with options)
-                var evt = new MouseEvent('click', {
-                    bubbles: true,
-                    cancelable: true,
-                    view: window
-                });
-                // If cancelled, don't dispatch our event
-                var canceled = !elem.dispatchEvent(evt);
-            };
-            // first click opens menu
-            simulateClick(hamburger);
+            // clicks menu icon
+            $('.menu-icon-link').trigger('click');
             // click removes 'menu-hidden' class from body and checks to make sure that it doesn't contain that class
-            expect(document.body.classList.contains('menu-hidden')).toBe(false);
+            expect($('body').hasClass('menu-hidden')).not.toBe(true);
             // second click closes menu
-            simulateClick(hamburger);
+            $('.menu-icon-link').trigger('click');
             // click adds 'menu-hidden' class to body and checks to make sure that class is there
-            expect(document.body.classList.contains('menu-hidden')).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
 // Test suite to check there loadFeed function works and loads at least one feed
     describe('Initial Entries', function() {
-        let feed = document.querySelector('.feed');
-        let entry = document.querySelector('.entry-link');
         // Runs the loadFeed function to establish baseline for test
         beforeEach(function(done) {
-            loadFeed(0, function() {
-            done();
-            });
+            loadFeed(0, done);
         });
         // loadFeed function should create a feed container that has article elements with '.entry' class
         // test checks to see if there is at least one article element
-        it('contains entry element in feed element', function() {
-            feed = document.querySelector('.feed');
-            entry = document.querySelector('.entry-link');
-            expect(feed.contains(entry)).toBe(true);
+        it('contains at least one article element with entry class in .feed div element', function() {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
         });
     });
 // Test suite to see if content changes when new feed is loaded
     describe('New Feed Selection', function() {
-        var firstFeedContent;
-        // Runs loadFeed function and sets the results to a variable
+        let firstFeedContent;
+        // let secondFeedContent;
         beforeEach(function(done) {
+        // Invokes loadFeed function with first feed in the allFeeds array
             loadFeed(0, function() {
-                firstFeedContent = document.querySelector('.feed').innerHTML;
+                // Sets results to variable
+                firstFeedContent = document.querySelector('.feed').innerText;
+                // Invokes loadFeed function with second feed in the allFeeds array
+                loadFeed(1, done);
             });
-            done();
         });
 
         it('loads new content upon new feed load', function() {
-            // Runs loadFeed function again
-            loadFeed(1, function() {
-            });
-            // Sets results to new variable
-            const secondFeedContent = document.querySelector('.feed').innerHTML;
-            // compares the results of both variables to see that there is a difference
-            expect(firstFeedContent).not.toBe(secondFeedContent);
+            // compares the result of the current feed to see that there is a difference with first feed
+            expect(document.querySelector('.feed').innerText).not.toBe(firstFeedContent);
         });
     });
 }());
